28ceafe6222ede17b702f401abd78959a65e8902
kauhan-gomes <80726721+kauhan-gomes@users.noreply.github.com>
2023-05-28 16:41:14 -0300

    Update plot_covid.py

--- plot_covid.py
+++ plot_covid.py
@@ -4,49 +4,48 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-#streamlit run codigoBase.py
-
-#LENDO O DATASET
+# Lendo o dataset
 df = pd.read_csv('https://raw.githubusercontent.com/wcota/covid19br/master/cases-brazil-states.csv')
 
-#MELHORANDO O NOME DAS COLUNAS DA TABELA
+# Melhorando o nome das colunas da tabela
 df = df.rename(columns={'newDeaths': 'Novos óbitos', 'newCases': 'Novos casos', 'deaths_per_100k_inhabitants': 'Óbitos por 100 mil habitantes', 'totalCases_per_100k_inhabitants': 'Casos por 100 mil habitantes'})
 
-#SELECÃO DO ESTADO
+# Selecionando o estado
 estados = list(df['state'].unique())
-state = st.selectbox('Qual o estado desejado?', estados)
+state = st.sidebar.selectbox('Selecione o estado', estados)
 
-#SELEÇÃO DO GRÁFICO
-graficos = ['Gráfico de Linha', 'Gráfico de Área', 'Gráfico de Barras']
-grafico_colunas = {
-    'Novos óbitos': ['Gráfico de Linha', 'Gráfico de Área', 'Gráfico de Barras'],
-    'Novos casos': ['Gráfico de Linha', 'Gráfico de Área', 'Gráfico de Barras'],
-    'Óbitos por 100 mil habitantes': ['Gráfico de Linha', 'Gráfico de Área', 'Gráfico de Barras'],
-    'Casos por 100 mil habitantes': ['Gráfico de Linha', 'Gráfico de Área', 'Gráfico de Barras']
-}
-grafico = st.selectbox('Qual tipo de gráfico?', graficos)
-
-#SELEÇÃO DA COLUNA
+# Selecionando o tipo de informação
 colunas = ['Novos óbitos', 'Novos casos', 'Óbitos por 100 mil habitantes', 'Casos por 100 mil habitantes']
-column = st.selectbox('Qual tipo de informação?', colunas)
+column = st.sidebar.selectbox('Selecione o tipo de informação', colunas)
 
-#SELEÇÃO DAS LINHAS QUE PERTECEM AO ESTADO
-df = df[df['state'] == state]
+# Selecionando o tipo de gráfico
+tipos_grafico = ['Gráfico de Linha', 'Gráfico de Área', 'Gráfico de Barras']
+tipo_grafico = st.sidebar.selectbox('Selecione o tipo de gráfico', tipos_grafico)
 
-if grafico == 'Gráfico de Linha':
-    fig = px.line(df, x="date", y=column, title=column + ' - ' + state)
-elif grafico == 'Gráfico de Área':
-    fig = px.area(df, x="date", y=column, title=column + ' - ' + state)
+# Filtrando os dados do estado selecionado
+df_estado = df[df['state'] == state]
+
+# Criando o gráfico
+if tipo_grafico == 'Gráfico de Linha':
+    fig = px.line(df_estado, x='date', y=column, title=column + ' - ' + state)
+elif tipo_grafico == 'Gráfico de Área':
+    fig = px.area(df_estado, x='date', y=column, title=column + ' - ' + state)
 else:
-    fig = px.bar(df, x="date", y=column, title=column + ' - ' + state)
+    fig = px.bar(df_estado, x='date', y=column, title=column + ' - ' + state)
 
+# Personalizando o layout do gráfico
 fig.update_layout(xaxis_title='Data', yaxis_title=column.upper(), title={'x': 0.5})
 
-st.title('DADOS COVID - BRASIL')
-st.write('Nessa aplicação, o usuário tem a opção de escolher o estado, o tipo de gráfico e o tipo de informação para mostrar o gráfico. Utilize o menu lateral para fazer as escolhas.')
+# Exibindo o título e o gráfico no Streamlit
+st.title('Dados COVID-19 - Brasil')
+st.write('Este sistema permite visualizar gráficos interativos com dados da COVID-19 no Brasil.')
+
+st.sidebar.markdown('---')
+st.sidebar.caption('Selecione as opções no menu lateral para atualizar o gráfico.')
 
 st.plotly_chart(fig, use_container_width=True)
 
-st.caption('Os dados foram obtidos a partir do site: https://github.com/wcota/covid19br')
+# Rodapé
+st.caption('Dados obtidos do repositório: https://github.com/wcota/covid19br')
 
 
